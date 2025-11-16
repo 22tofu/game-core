5485a565d94b2de0af29017e1476c1f606cdadf1
Ron Talledo <Rontalledo@gmail.com>
2025-11-16 13:20:42 +0800

    add colormode

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
-    <HStack>
+    <HStack justifyContent={'space-between'} paddingX={"10px"}>
         <Image src={logo} alt="Logo" boxSize="60px" />
         <Text fontSize="2xl" fontWeight="bold">Game Core</Text>
+        <ColorModeSwitch />
     </HStack>
   )
 }
