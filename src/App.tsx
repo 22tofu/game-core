08c808c83d5871d4b3bda6fca3f4a8b938cfeebf
Ron Talledo <Rontalledo@gmail.com>
2025-11-20 21:55:40 +0800

    build game cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { Button, Grid, GridItem, Show } from '@chakra-ui/react'
-import NavBar from './components/NavBar'
-import GameGrid from './components/GameGrid'
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
+import GameGrid from "./components/GameGrid";
 function App() {
-  return <Grid templateAreas={{
-    base: `"nav" "main"`,
-    lg: `"nav nav" "aside main"`,
-  }}>
-    <GridItem area="nav">
-      <NavBar />
-    </GridItem>
-    <Show above='lg'>
-      <GridItem area="aside">Aside</GridItem>
-    </Show>
-    <GridItem area="main">
-      <GameGrid/>
-    </GridItem>
-    
-  </Grid>
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" />
+      </Show>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
